29bda4f9266cc05ce5c0ae195f5edebcd6b1c88b
nour ahmed <116553375+nourabbasHRA@users.noreply.github.com>
2023-05-12 09:35:15 +0300

    Adding ng zorro

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,27 @@
 import { NgModule } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
-
-import { AppComponent } from "./app.component";
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
 import { RouterOutlet } from "@angular/router";
 import {AppRoutingModule} from "./app-routing.module";
 
+import { NzLayoutModule } from 'ng-zorro-antd/layout';
+import {NzBreadCrumbModule} from "ng-zorro-antd/breadcrumb";
+import {NzDropDownModule} from "ng-zorro-antd/dropdown";
+
+import { AppComponent } from "./app.component";
+import {HttpClientModule} from "@angular/common/http";
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, RouterOutlet, AppRoutingModule],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule ,
+    HttpClientModule,
+    RouterOutlet,
+    AppRoutingModule,
+    NzLayoutModule,
+    NzBreadCrumbModule,
+    NzDropDownModule],
   providers: [],
   bootstrap: [AppComponent],
 })
